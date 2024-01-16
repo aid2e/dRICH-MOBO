304cd4c6abdbdf477c921e8c282ce11dba592c24
cpecar <pecarc@duq.edu>
2024-01-16 09:53:09 -0500

    add checks for if eic-shell and epic are installed

--- MOBO-tools/setup.sh
+++ MOBO-tools/setup.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-export EIC_SHELL_HOME=$(pwd)
-export EPIC_HOME=$(pwd)/epic/
+
+EPIC_DIR=$(pwd)/epic/
+if [ ! -d "$EPIC_DIR" ]; then
+    echo "local ePIC source code not found, downloading"
+    git clone "https://github.com/eic/epic.git"
+    EPIC_DIR=$(pwd)/epic/
+else
+    echo "ePIC source code found"
+fi
+
+EIC_SHELL_DIR=$(pwd)
+if [ ! -f "$EIC_SHELL_DIR/eic-shell" ]; then
+    echo "eic-shell not found, downloading"
+    curl --location https://get.epic-eic.org | bash
+    EIC_SHELL_DIR=$(pwd)
+else
+    echo "eic-shell found"
+fi
+
+export EIC_SHELL_HOME=$EIC_SHELL_DIR
+export EPIC_HOME=$EPIC_DIR
 export EPIC_MOBO_UTILS=$(pwd)/ProjectUtils/ePICUtils/
