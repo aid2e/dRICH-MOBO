90196e4d2909245ce64f8551e4f2d9d307ab8f47
Connor Pecar <cmp115@dcc-login-03.oit.duke.edu>
2024-02-22 12:33:48 -0500

    supply output directory for sim results

--- MOBO-tools/ProjectUtils/ePICUtils/shell_wrapper_job.sh
+++ MOBO-tools/ProjectUtils/ePICUtils/shell_wrapper_job.sh
@@ -15,15 +15,15 @@ cd $EPIC_HOME
 source install/setup.sh
 cd "$current_dir"
 $EPIC_MOBO_UTILS/genMomentumScan_job.sh $1 $2 $3 $4 "kaon+" $6
-$EPIC_MOBO_UTILS/dRICHAna recon_scan_$6_kaon+_p_$1_eta_$2_$3.root recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt $5
+$EPIC_MOBO_UTILS/dRICHAna recon_scan_$6_kaon+_p_$1_eta_$2_$3.root recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt $AIDE_HOME/log/results/ $5
 $EPIC_MOBO_UTILS/genMomentumScan_job.sh $1 $2 $3 $4 "pi+" $6
-$EPIC_MOBO_UTILS/dRICHAna recon_scan_$6_pi+_p_$1_eta_$2_$3.root recon_scan_$6_pi+_p_$1_eta_$2_$3.txt $5
+$EPIC_MOBO_UTILS/dRICHAna recon_scan_$6_pi+_p_$1_eta_$2_$3.root recon_scan_$6_pi+_p_$1_eta_$2_$3.txt $AIDE_HOME/log/results/  $5
 EOF
 
-python $EPIC_MOBO_UTILS/getNsigma.py recon_scan_$6_pi+_p_$1_eta_$2_$3.txt recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt
+#python $EPIC_MOBO_UTILS/getNsigma.py recon_scan_$6_pi+_p_$1_eta_$2_$3.txt recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt
 
-rm recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt
-rm recon_scan_$6_pi+_p_$1_eta_$2_$3.txt
+#rm recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt
+#rm recon_scan_$6_pi+_p_$1_eta_$2_$3.txt
 rm recon_scan_$6_kaon+_p_$1_eta_$2_$3.root
 rm recon_scan_$6_pi+_p_$1_eta_$2_$3.root
 rm scan_$6_kaon+_p_$1_eta_$2_$3.root
