9c64b14566cecef49c2625f5d5cf0dd267eb481c
Connor Pecar <cmp115@dcc-login-02.oit.duke.edu>
2024-03-20 13:26:13 -0400

    keep only dRICH relevant banks from eicrecon

--- MOBO-tools/ProjectUtils/ePICUtils/genMomentumScan_job.sh
+++ MOBO-tools/ProjectUtils/ePICUtils/genMomentumScan_job.sh
@@ -6,4 +6,6 @@ if [ "$#" != 6 ]; then
 fi
 
 npsim --compactFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}_$6.xml -G -N $4 --gun.etaMax $3  --gun.etaMin $2 --gun.momentumMax "$1*GeV" --gun.momentumMin "$1*GeV" --gun.particle $5 --gun.distribution "uniform" -O $AIDE_HOME/log/sim_files/scan_$6_$5_p_$1_eta_$2_$3.root
-eicrecon $AIDE_HOME/log/sim_files/scan_$6_$5_p_$1_eta_$2_$3.root -Ppodio:output_file=$AIDE_HOME/log/sim_files/recon_scan_$6_$5_p_$1_eta_$2_$3.root -Pdd4hep:xml_files=${DETECTOR_PATH}/${DETECTOR_CONFIG}_$6.xml
+echo "done with npsim"
+eicrecon $AIDE_HOME/log/sim_files/scan_$6_$5_p_$1_eta_$2_$3.root -Ppodio:output_file=$AIDE_HOME/log/sim_files/recon_scan_$6_$5_p_$1_eta_$2_$3.root -Pdd4hep:xml_files=${DETECTOR_PATH}/${DETECTOR_CONFIG}_$6.xml -Ppodio:output_include_collections=DRICHHits,MCParticles,DRICHRawHits,DRICHRawHitsAssociations,DRICHAerogelTracks,DRICHGasTracks,DRICHAerogelIrtCherenkovParticleID,DRICHGasIrtCherenkovParticleID,DRICHMergedIrtCherenkovParticleID
+echo "done with eicrecon"
