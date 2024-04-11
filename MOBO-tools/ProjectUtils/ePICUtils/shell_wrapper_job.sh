af71efdf5b53b2a3adcb031a5b8cc2a2304e0848
Connor Pecar <cmp115@dcc-login-05.oit.duke.edu>
2024-04-11 15:20:37 -0400

    particle to throw as arg to shell wrapper

--- MOBO-tools/ProjectUtils/ePICUtils/shell_wrapper_job.sh
+++ MOBO-tools/ProjectUtils/ePICUtils/shell_wrapper_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-if [ "$#" -ne 6 ]; then
-    echo "Usage: $0 <p> <etamin> <etamax> <n particles> <radiator> <job id>"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $0 <p> <etamin> <etamax> <n particles> <radiator> <job id> <particle>"
     exit 1
 fi
 
@@ -12,18 +12,15 @@ current_dir=$(pwd)
 # produce pi+ and K+ sample
 cat << EOF | $EIC_SHELL_HOME/eic-shell
 source $AIDE_HOME/load_epic.sh
-$EPIC_MOBO_UTILS/genMomentumScan_job.sh $1 $2 $3 $4 "kaon+" $6
-$EPIC_MOBO_UTILS/dRICHAna $AIDE_HOME/log/sim_files/recon_scan_$6_kaon+_p_$1_eta_$2_$3.root recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt $AIDE_HOME/log/results/ $5
-$EPIC_MOBO_UTILS/genMomentumScan_job.sh $1 $2 $3 $4 "pi+" $6
-$EPIC_MOBO_UTILS/dRICHAna $AIDE_HOME/log/sim_files/recon_scan_$6_pi+_p_$1_eta_$2_$3.root recon_scan_$6_pi+_p_$1_eta_$2_$3.txt $AIDE_HOME/log/results/  $5
+$EPIC_MOBO_UTILS/genMomentumScan_job.sh $1 $2 $3 $4 $7 $6
+$EPIC_MOBO_UTILS/dRICHAna $AIDE_HOME/log/sim_files/recon_scan_$6_$7_p_$1_eta_$2_$3.root recon_scan_$6_$7_p_$1_eta_$2_$3.txt $AIDE_HOME/log/results/ $5
 EOF
 
 #python $EPIC_MOBO_UTILS/getNsigma.py recon_scan_$6_pi+_p_$1_eta_$2_$3.txt recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt
 
 #rm recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt
 #rm recon_scan_$6_pi+_p_$1_eta_$2_$3.txt
-#rm $AIDE_HOME/log/sim_files/recon_scan_$6_kaon+_p_$1_eta_$2_$3.root
-#rm $AIDE_HOME/log/sim_files/recon_scan_$6_pi+_p_$1_eta_$2_$3.root
-#rm $AIDE_HOME/log/sim_files/scan_$6_kaon+_p_$1_eta_$2_$3.root
+rm $AIDE_HOME/log/sim_files/recon_scan_$6_$7_p_$1_eta_$2_$3.root
+rm $AIDE_HOME/log/sim_files/scan_$6_$7_p_$1_eta_$2_$3.root
 #rm $AIDE_HOME/log/sim_files/scan_$6_pi+_p_$1_eta_$2_$3.root
 
