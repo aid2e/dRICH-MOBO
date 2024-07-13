0f0656c8e0eca2c80ca302db1864fb0a3fd47ba5
Ian-Kim0 <ian.kim@duke.edu>
2024-07-13 00:09:19 -0400

    Change SEM for metrics from 0 to being inferred by ax

--- MOBO-tools/ProjectUtils/metric_utilities.py
+++ MOBO-tools/ProjectUtils/metric_utilities.py
@@ -20,7 +20,8 @@ class SlurmJobMetric(Metric):  # Pulls data for trial from external system.
                 trial.run_metadata.get("job_id")
             )
             #TODO: get real uncertainty/noise estimate
-            sem = 0.0
+            #Set sem to None for now to let ax infer it
+            sem = None
             df_dict = {
                 "trial_index": trial.index,
                 "metric_name": self.name,
