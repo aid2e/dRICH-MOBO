230cc5919a3fed89ef2a0886e67306eeb34e2b10
Ian-Kim0 <ian.kim@duke.edu>
2024-07-16 14:49:36 -0400

    Print particle and job number after finishing ddsim run

--- MOBO-tools/ProjectUtils/ePICUtils/genMomentumScan_job.sh
+++ MOBO-tools/ProjectUtils/ePICUtils/genMomentumScan_job.sh
@@ -6,4 +6,4 @@ if [ "$#" != 4 ]; then
 fi
 
 ddsim --compactFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}_$4.xml --runType "batch" -G -N $2 --gun.particle $3 --gun.momentumMin "$1*GeV" --gun.momentumMax "$1*GeV" --gun.thetaMin "78*deg" --gun.thetaMax "102*deg" --gun.distribution "uniform" --physics.list "FTFP_BERT" --part.userParticleHandler="" --outputFile $AIDE_HOME/log/sim_files/scan_$4_$3_p_$1.edm4hep.root
-echo "done with ddsim"
+echo "done with ddsim ($3 job #$4)"
