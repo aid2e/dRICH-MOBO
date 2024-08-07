75711775b8fc74ca340d04f4a47c7a22822a54af
Connor Pecar <cmp115@dcc-login-04.oit.duke.edu>
2024-08-07 12:54:19 -0400

    Update how we compute low/high \eta objectives; if any job failed, make the trial fail (TODO: should we try to restart the failed jobs?)

--- MOBO-tools/ProjectUtils/ePICUtils/runTestsAndObjectiveCalc.py
+++ MOBO-tools/ProjectUtils/ePICUtils/runTestsAndObjectiveCalc.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os, sys, subprocess, time
 import math
+import uncertainties
 
 #NEEDS TO:
 # 1. run overlap check
@@ -43,11 +44,15 @@ class SubJobManager:
             file.write("#!/bin/bash\n")
             file.write("#SBATCH --job-name=drich-mobo\n")
             file.write("#SBATCH --account=vossenlab\n")
-            file.write("#SBATCH --partition=common\n")
+            file.write("#SBATCH --partition=scavenger\n")
+            #file.write("#SBATCH --partition=vossenlab-gpu\n")
+            #file.write("#SBATCH --nodelist=dcc-vossenlab-gpu-0[1-4]\n")
+            file.write("#SBATCH --nodes=1\n")
+            file.write("#SBATCH --cpus-per-task=2\n")
             file.write("#SBATCH --mem=2G\n")
-            file.write("#SBATCH --time=2:00:00\n")
-            file.write("#SBATCH --output={}/drich-mobo-subjob_%j.out\n".format(str(os.environ["AIDE_HOME"])+"/log/job_output"))
-            file.write("#SBATCH --error={}/drich-mobo-subjob_%j.err\n".format(str(os.environ["AIDE_HOME"])+"/log/job_output"))
+            file.write("#SBATCH --time=3:45:00\n")
+            file.write("#SBATCH --output={}/drich-mobo-subjob_%j.out\n".format("/cwork/cmp115/AID2E/dRICH-MOBO/log/job_output/"))
+            file.write("#SBATCH --error={}/drich-mobo-subjob_%j.err\n".format("/cwork/cmp115/AID2E/dRICH-MOBO/log/job_output/"))
             
             file.write(str(os.environ["EPIC_MOBO_UTILS"])+"/shell_wrapper_job.sh {} {} {} {} {} {} {} \n".format(p,eta_min,eta_max,self.n_part,radiator,self.job_id,particle))
         return filename
@@ -119,7 +124,7 @@ class SubJobManager:
         # executed when we have overlaps and want to punish this result,
         # but the trial didn't exactly "fail"
         # TODO: is this how we want to treat this?
-        final_results = np.array( [0, 0, 0] )
+        final_results = np.array( [0, 0, 0, 0, 0, 0] )
         np.savetxt(self.outname,final_results)
         return
     def retrieveResults(self):
@@ -128,12 +133,15 @@ class SubJobManager:
         results_nsigma = []
         results_eff = []
         result_p = []
+        result_etalow = []
         for i in range(len(self.p_eta_points)):
             p_eta_point = self.p_eta_points[i]
+            print("p_eta_point: ", p_eta_point)
             p = p_eta_point[0]
             eta_min = p_eta_point[1][0]
             eta_max = p_eta_point[1][1]
             if self.final_job_status[i] == 1:
+                print("job status 1")
                 K_plus_cher = np.loadtxt(str(os.environ["AIDE_HOME"])+"/log/results/"+"recon_scan_{}_kaon+_p_{}_eta_{}_{}.txt".format(self.job_id,p,eta_min,eta_max))
                 pi_plus_cher = np.loadtxt(str(os.environ["AIDE_HOME"])+"/log/results/"+"recon_scan_{}_pi+_p_{}_eta_{}_{}.txt".format(self.job_id,p,eta_min,eta_max))
                 mean_nphot = (pi_plus_cher[0] + K_plus_cher[0])/2
@@ -145,36 +153,75 @@ class SubJobManager:
                     nsigma = 0
                 #get mean fraction of tracks with reco photons
                 mean_eff = (pi_plus_cher[3] + K_plus_cher[3])/2
-                results_nsigma.append(nsigma)
-                results_eff.append(mean_eff)
+                results_nsigma.append(uncertainties.ufloat(nsigma,p_eta_point[3]*nsigma))
+                results_eff.append(uncertainties.ufloat(mean_eff,p_eta_point[4]*mean_eff))
                 result_p.append(p)
-        # mean to reduce to 2 momentum points
-        # TODO: write a function to do this based on some weighting from physics multiplicities
+                result_etalow.append(eta_min)
+
         result_p = np.array(result_p)
+        result_etalow = np.array(result_etalow)
         results_nsigma = np.array(results_nsigma)
-        results_eff = np.array(results_eff)
+        results_eff = np.array(results_eff)        
+
+        def harmonic_mean(values,weights):
+            denom = 0
+            if np.any(values==0):
+                return uncertainties.ufloat(0,0)
+            for (weight,value) in zip(weights,values):
+                denom += weight/value
+            return np.sum(weights)/(denom)
+        # average together low/mid and mid/high bins (reduce dimensions but still have some info
+        # from mid \eta)
+        sep_etalow = harmonic_mean( np.array([results_nsigma[0],results_nsigma[3]]), np.array([1.,1.]) )
+        sep_etamid = harmonic_mean( np.array([results_nsigma[1],results_nsigma[4]]), np.array([1.,1.]) )
+        sep_etahigh = harmonic_mean( np.array([results_nsigma[2],results_nsigma[5]]), np.array([1.,1.]) )
+
+        mean_sep_low = harmonic_mean( np.array([sep_etalow,sep_etamid]),np.array([1.0,0.5]))
+        mean_sep_high = harmonic_mean( np.array([sep_etamid,sep_etahigh]),np.array([0.5,1.0]))
         
-        if np.sum(result_p==15) > 0 and np.sum(result_p==40) > 0:
-            final_results = np.array( [ np.mean( results_nsigma[np.where(result_p==15)] ),
-                                        np.mean( results_nsigma[np.where(result_p==40)] ),
-                                        np.mean( results_eff ) 
-                                       ] )
-        else:
-            # if all jobs failed for a momentum point, exit failed
-            # TODO: is this how we want to treat this case?
-            sys.exit(1)
+        acc_all = np.mean( results_eff )
+        
+        final_results = np.array( [ mean_sep_low.n, mean_sep_low.s,
+                                    mean_sep_high.n, mean_sep_high.s,
+                                    acc_all.n, acc_all.s
+                                   ]
+                                )
+        
         np.savetxt(self.outname,final_results)
         return
 
-nobj = 6
-npart = 100
+npart = 1500
+# [momentum, eta range, radiator, dev_piKsep, dev_acc]
+# std dev for 2500 tracks
+'''
 p_eta_scan = [
-    [15, [1.3,2.0], 0],
-    [15, [2.0,2.5], 0],
-    [15, [2.5,3.5], 0],
-    [40, [1.3,2.0], 1],
-    [40, [2.0,2.5], 1],
-    [40, [2.5,3.5], 1]
+    [15, [1.3,2.0], 0, 0.01992527, 0.00700383],
+    [15, [2.0,2.5], 0, 0.01385765, 0.00723669],
+    [15, [2.5,3.5], 0, 0.01577519, 0.00987238],
+    [40, [1.3,2.0], 1, 0.03229172, 0.00645222],
+    [40, [2.0,2.5], 1, 0.01007051, 0.00276879],
+    [40, [2.5,3.5], 1, 0.0106865, 0.00272314]
+]
+'''
+# std dev for 2000 tracks
+'''
+p_eta_scan = [    
+    [15, [1.5,2.0], 0, 0.02242128, 0.00802887],
+    [15, [2.0,2.5], 0, 0.01539502, 0.00788831],
+    [15, [2.5,3.5], 0, 0.01757623, 0.00998055],
+    [40, [1.5,2.0], 1, 0.03879622, 0.00747648],
+    [40, [2.0,2.5], 1, 0.01182995, 0.00298889],
+    [40, [2.5,3.5], 1, 0.01211408, 0.00309565]
+]
+'''
+# std dev for 1500 tracks
+p_eta_scan = [    
+    [15, [1.5,2.0], 0, 0.02457205, 0.00878185],
+    [15, [2.0,2.5], 0, 0.01871374, 0.00916126],
+    [15, [2.5,3.5], 0, 0.02046443, 0.01240257],
+    [40, [1.5,2.0], 1, 0.04405797, 0.00824205],
+    [40, [2.0,2.5], 1, 0.01390604, 0.00350744],
+    [40, [2.5,3.5], 1, 0.01391206, 0.00349814]
 ]
 
 jobid = sys.argv[1]
@@ -194,9 +241,12 @@ print("no overlaps, starting momentum/eta scan jobs")
 
 manager.runJobs()
 manager.monitorJobs()
-if np.sum(manager.final_job_status) <= 0:
-    manager.writeFailedObjectives()
-    print("something wrong with all jobs")
+
+manager.retrieveResults()
+if np.sum(manager.final_job_status) < 6:
+    #manager.writeFailedObjectives()
+    sys.exit(1)
+    print("some job failed, flag as failure")
 else:
     manager.retrieveResults()
     print("successfully retrieved results")
