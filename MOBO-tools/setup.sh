e166fe4e9704ff6c648582104a3a3bcdcd875350
Connor Pecar <cmp115@dcc-login-02.oit.duke.edu>
2025-02-13 13:43:22 -0500

    Remove unused environment variables; stop tracking old unused scripts

--- MOBO-tools/setup.sh
+++ MOBO-tools/setup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 
-EPIC_DIR=$(pwd)/epic-geom-drich-mobo/
-EIC_SHELL_DIR=$(pwd)
-EICRECON_DIR=$(pwd)/EICrecon-drich-mobo/
-
-export EIC_SHELL_HOME=$EIC_SHELL_DIR
-export EPIC_HOME=$EPIC_DIR
-
-export DETECTOR_PATH=$EPIC_HOME
-export DETECTOR_CONFIG=epic_craterlake
-
+export EIC_SHELL_HOME=$(pwd)
 export EPIC_MOBO_UTILS=$(pwd)/ProjectUtils/ePICUtils/
 export AIDE_HOME=$(pwd)
 
