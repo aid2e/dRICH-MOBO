8deeb5a0198f4cec519212e164ab805dfaf1dbdf
Ian-Kim0 <ian.kim@duke.edu>
2025-03-12 16:10:27 -0400

    Migrate project from conda to uv

--- MOBO-tools/run_optimization.sh
+++ MOBO-tools/run_optimization.sh
@@ -1,4 +1,2 @@
-conda activate mobo
 source setup.sh
-python wrapper_slurm_basic.py -c optimize.config -d parameters.config
-conda deactivate
+uv run python wrapper_slurm_basic.py -c optimize.config -d parameters.config
