037942cf95a1bcfdf06ccfb408150a9467cdb628
Wen Guan <wguan.icedew@gmail.com>
2025-04-25 17:10:06 +0200

    update to use new container

--- tools/build.sh
+++ tools/build.sh
@@ -5,23 +5,26 @@ curl --location https://get.epic-eic.org | bash
 # git clone -b v1.15-drich-3mirror https://github.com/cpecar/EICrecon-drich-mobo.git
 # git clone -b 24.07.0-drich-3mirror https://github.com/cpecar/epic-geom-drich-mobo.git
 # git clone -b multi-mirror-irt https://github.com/eic/irt.git
+
 git clone -b v1.19-drich-2mirror https://github.com/cpecar/EICrecon-drich-mobo.git
 git clone -b 24.11.1-drich-2mirror https://github.com/cpecar/epic-geom-drich-mobo.git
 git clone -b multi-mirror-irt https://github.com/eic/irt.git
-
 
 source setup_new.sh
 
 # run eic-shell to setup singularity
 # bash eic-shell
-./eic-shell -c jug_xl -v 24.08.1-stable
+# ./eic-shell -c jux_xl -v 24.08.1-stable
+
+./eic-shell -c eic_xl -v 24.11.1-stable
 
 # build
-bash build_epic.sh epic-geom-drich-mobo/ $EIC_SOFTWARE/
-bash build_irt.sh irt/ $EIC_SOFTWARE/
+./build_epic.sh epic-geom-drich-mobo $EIC_SOFTWARE
 
-source  eic-software/setup.sh
-bash build_eicrecon.sh EICrecon-drich-mobo/ $EIC_SOFTWARE/
+./build_irt.sh irt $EIC_SOFTWARE
+
+# source  eic-software/setup.sh
+./build_eicrecon.sh EICrecon-drich-mobo $EIC_SOFTWARE
 
 #
 cd ProjectUtils/ePICUtils
