0b97a6b258cc81e8d9679c4cbcdbcfaff611bc80
Connor Pecar <cmp115@dcc-login-05.oit.duke.edu>
2025-05-27 14:55:58 -0400

    Add environment variable for where to store (temporary) simulation files for each trial

--- MOBO-tools/ProjectUtils/ePICUtils/genMomentumScan_job.sh
+++ MOBO-tools/ProjectUtils/ePICUtils/genMomentumScan_job.sh
@@ -5,7 +5,7 @@ if [ "$#" != 6 ]; then
     exit 1
 fi
 
-npsim --compactFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}_$6.xml -G -N $4 --gun.etaMax $3  --gun.etaMin $2 --gun.phiMin "0" --gun.phiMax "6.2831853" --gun.momentumMax "$1*GeV" --gun.momentumMin "$1*GeV" --gun.particle $5 --gun.distribution "eta" -O $AIDE_HOME/log/sim_files/scan_$4_$6_$5_p_$1_eta_$2_$3.root
+npsim --compactFile ${DETECTOR_PATH}/${DETECTOR_CONFIG}_$6.xml -G -N $4 --gun.etaMax $3  --gun.etaMin $2 --gun.phiMin "0" --gun.phiMax "6.2831853" --gun.momentumMax "$1*GeV" --gun.momentumMin "$1*GeV" --gun.particle $5 --gun.distribution "eta" -O $SIM_OUTPUT/scan_$4_$6_$5_p_$1_eta_$2_$3.root
 echo "done with npsim"
-eicrecon $AIDE_HOME/log/sim_files/scan_$4_$6_$5_p_$1_eta_$2_$3.root -Ppodio:output_file=$AIDE_HOME/log/sim_files/recon_scan_$4_$6_$5_p_$1_eta_$2_$3.root -Pdd4hep:xml_files=${DETECTOR_PATH}/${DETECTOR_CONFIG}_$6.xml -Ppodio:output_include_collections=DRICHHits,MCParticles,DRICHRawHits,DRICHRawHitsAssociations,DRICHAerogelTracks,DRICHGasTracks,DRICHAerogelIrtCherenkovParticleID,DRICHGasIrtCherenkovParticleID,DRICHMergedIrtCherenkovParticleID
+eicrecon $SIM_OUTPUT/scan_$4_$6_$5_p_$1_eta_$2_$3.root -Ppodio:output_file=$SIM_OUTPUT/recon_scan_$4_$6_$5_p_$1_eta_$2_$3.root -Pdd4hep:xml_files=${DETECTOR_PATH}/${DETECTOR_CONFIG}_$6.xml -Ppodio:output_include_collections=DRICHHits,MCParticles,DRICHRawHits,DRICHRawHitsAssociations,DRICHAerogelTracks,DRICHGasTracks,DRICHAerogelIrtCherenkovParticleID,DRICHGasIrtCherenkovParticleID,DRICHMergedIrtCherenkovParticleID
 echo "done with eicrecon"
--- MOBO-tools/ProjectUtils/ePICUtils/shell_wrapper_job.sh
+++ MOBO-tools/ProjectUtils/ePICUtils/shell_wrapper_job.sh
@@ -13,14 +13,11 @@ current_dir=$(pwd)
 cat << EOF | $EIC_SHELL_HOME/eic-shell
 source $AIDE_HOME/load_epic.sh
 $EPIC_MOBO_UTILS/genMomentumScan_job.sh $1 $2 $3 $4 $7 $6
-$EPIC_MOBO_UTILS/dRICHAna $AIDE_HOME/log/sim_files/recon_scan_$4_$6_$7_p_$1_eta_$2_$3.root recon_scan_$4_$6_$7_p_$1_eta_$2_$3.txt $AIDE_HOME/log/results/ $5
+$EPIC_MOBO_UTILS/dRICHAna $SIM_OUTPUT/recon_scan_$4_$6_$7_p_$1_eta_$2_$3.root recon_scan_$4_$6_$7_p_$1_eta_$2_$3.txt $AIDE_HOME/log/results/ $5
 EOF
 
 #python $EPIC_MOBO_UTILS/getNsigma.py recon_scan_$6_pi+_p_$1_eta_$2_$3.txt recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt
 
-#rm recon_scan_$6_kaon+_p_$1_eta_$2_$3.txt
-#rm recon_scan_$6_pi+_p_$1_eta_$2_$3.txt
-rm $AIDE_HOME/log/sim_files/recon_scan_$4_$6_$7_p_$1_eta_$2_$3.root
-rm $AIDE_HOME/log/sim_files/scan_$4_$6_$7_p_$1_eta_$2_$3.root
-#rm $AIDE_HOME/log/sim_files/scan_$6_pi+_p_$1_eta_$2_$3.root
+#rm $AIDE_HOME/log/sim_files/recon_scan_$4_$6_$7_p_$1_eta_$2_$3.root
+#rm $AIDE_HOME/log/sim_files/scan_$4_$6_$7_p_$1_eta_$2_$3.root
 
