ea1d51349fb503e57fd5baaa09188a11c77c7478
Connor Pecar <cmp115@dcc-login-01.oit.duke.edu>
2025-05-28 14:31:17 -0400

    Make default simulation output MOBO-tools/log/sim_files/

--- MOBO-tools/setup.sh
+++ MOBO-tools/setup.sh
@@ -3,7 +3,7 @@
 
 export EIC_SHELL_HOME=$(pwd)
 export EPIC_MOBO_UTILS=$(pwd)/ProjectUtils/ePICUtils/
-export SIM_OUTPUT=/cwork/cmp115/AIDE/dRICH-MOBO/log/sim_files/
+export SIM_OUTPUT=$(pwd)/log/sim_files/
 export AIDE_HOME=$(pwd)
 
 export EIC_SOFTWARE=$(pwd)/eic-software/
